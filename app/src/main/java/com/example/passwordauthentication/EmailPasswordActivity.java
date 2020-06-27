cb3d2bac4be6ad28a088bb8a5a3ec5e3db907265
Vincent Li <li.vincent@gmail.com>
2020-06-27 13:57:15 -0700

    Updated UI to be dynamic

--- app/src/main/java/com/example/passwordauthentication/EmailPasswordActivity.java
+++ app/src/main/java/com/example/passwordauthentication/EmailPasswordActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -23,6 +24,14 @@ public class EmailPasswordActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private FirebaseUser user;
 
+    public EditText emailField;
+    public EditText pwField;
+    public Button register;
+    public Button signIn;
+    public Button signOut;
+    public TextView emailPrompt;
+    public TextView pwPrompt;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,8 +46,15 @@ public class EmailPasswordActivity extends AppCompatActivity {
         super.onStart();
         // check if user is signed in
         user = mAuth.getCurrentUser();
+        register = findViewById(R.id.button_register);
+        signIn = findViewById(R.id.button_sign_in);
+        signOut = findViewById(R.id.button_sign_out);
+        emailField = findViewById(R.id.editText_email);
+        pwField = findViewById(R.id.editText_pw);
+        emailPrompt = findViewById(R.id.textView);
+        pwPrompt = findViewById(R.id.textView2);
         setStatus();
-
+        updateUI();
     }
 
     private void createAccount(String email, String password) {
@@ -51,12 +67,16 @@ public class EmailPasswordActivity extends AppCompatActivity {
                         if (task.isSuccessful()) {
                             // Sign in success, update UI with the signed-in user's information
                             Log.d(TAG, "createUserWithEmail:success");
+                            // set user
                             user = mAuth.getCurrentUser();
+                            // alert
                             Toast.makeText(EmailPasswordActivity.this, "Create user successful.", Toast.LENGTH_SHORT).show();
+                            // update view
                             setStatus();
+                            updateUI();
                             refresh();
-
-                        } else {
+                        }
+                        else {
                             // If sign in fails, display a message to the user.
                             Log.w(TAG, "createUserWithEmail:failure", task.getException());
                             Toast.makeText(EmailPasswordActivity.this, "Create user failed.", Toast.LENGTH_SHORT).show();
@@ -77,12 +97,16 @@ public class EmailPasswordActivity extends AppCompatActivity {
                         if (task.isSuccessful()) {
                             // Sign in success, update UI with the signed-in user's information
                             Log.d(TAG, "signInWithEmail:success");
+                            // set user
                             user = mAuth.getCurrentUser();
+                            // alert
                             Toast.makeText(EmailPasswordActivity.this, "Authentication succeeded.", Toast.LENGTH_SHORT).show();
+                            // update view
                             setStatus();
+                            updateUI();
                             refresh();
-
-                        } else {
+                        }
+                        else {
                             // If sign in fails, display a message to the user.
                             Log.w(TAG, "signInWithEmail:failure", task.getException());
                             Toast.makeText(EmailPasswordActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
@@ -95,13 +119,12 @@ public class EmailPasswordActivity extends AppCompatActivity {
     private void signOut() {
         mAuth.signOut();
         setStatus();
+        updateUI();
         refresh();
     }
 
     //@Override
     public void onClick(View v) {
-        EditText emailField = findViewById(R.id.editText_email);
-        EditText pwField = findViewById(R.id.editText_pw);
         int i = v.getId();
         if(i == R.id.button_register) {
             if(!emailField.getText().toString().trim().matches("") && !pwField.getText().toString().trim().matches(""))
@@ -132,6 +155,27 @@ public class EmailPasswordActivity extends AppCompatActivity {
         Intent intent = getIntent();
         finish();
         startActivity(intent);
+    }
+
+    public void updateUI() {
+        if(user == null) {  // show sign-in and register components
+            emailPrompt.setVisibility(View.VISIBLE);
+            pwPrompt.setVisibility(View.VISIBLE);
+            emailField.setVisibility(View.VISIBLE);
+            pwField.setVisibility(View.VISIBLE);
+            register.setVisibility(View.VISIBLE);
+            signIn.setVisibility(View.VISIBLE);
+            signOut.setVisibility(View.GONE);
+        }
+        else {  // hide those components
+            emailPrompt.setVisibility(View.GONE);
+            pwPrompt.setVisibility(View.GONE);
+            emailField.setVisibility(View.GONE);
+            pwField.setVisibility(View.GONE);
+            register.setVisibility(View.GONE);
+            signIn.setVisibility(View.GONE);
+            signOut.setVisibility(View.VISIBLE);
+        }
     }
 
 }
